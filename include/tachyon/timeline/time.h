98b06ed30554a6151ea6c9a33f47c698c74a3fa1
PierThatDev <futurimilionariposta@gmail.com>
2026-04-19 11:35:08 +0200

    feat(scene): add frame and time sampling utilities

---
+++ include/tachyon/timeline/time.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "tachyon/spec/scene_spec.h"
+
+#include <cstdint>
+
+namespace tachyon::timeline {
+
+struct FrameSample {
+    std::int64_t frame_number{0};
+    double composition_time_seconds{0.0};
+};
+
+[[nodiscard]] double frame_to_seconds(const FrameRate& frame_rate, std::int64_t frame_number) noexcept;
+[[nodiscard]] FrameSample sample_frame(const FrameRate& frame_rate, std::int64_t frame_number) noexcept;
+[[nodiscard]] double local_time_from_composition(double composition_time_seconds, double start_time_seconds) noexcept;
+
+} // namespace tachyon::timeline
