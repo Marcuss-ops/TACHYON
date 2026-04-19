9b5f25742e273319ee0ad1ba64bfed9e7ef37780
PierThatDev <futurimilionariposta@gmail.com>
2026-04-19 11:12:32 +0200

    feat(native): track help requests in cli options

---
+++ include/tachyon/core/cli_options.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "tachyon/runtime/diagnostics.h"
+
+#include <filesystem>
+#include <string>
+
+namespace tachyon {
+
+struct CliOptions {
+    std::string command;
+    std::filesystem::path scene_path;
+    std::filesystem::path job_path;
+    std::filesystem::path output_override;
+    bool json_output{false};
+    bool help_requested{false};
+};
+
+ParseResult<CliOptions> parse_cli_options(int argc, char** argv);
+
+} // namespace tachyon
