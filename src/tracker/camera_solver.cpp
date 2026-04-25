fffa714db5d044dd15aeeae92391b9e4a7ed038b
Pierone <pierone@test.com>
2026-04-25 08:04:25 +0000

    chore: restore original files after modularization revert, keep cmake green

--- src/tracker/camera_solver.cpp
+++ src/tracker/camera_solver.cpp
@@ -1,10 +1,8 @@
 #include "tachyon/tracker/camera_solver.h"
-#include "camera_solver_keyframes.h"
-#include "camera_solver_math.h"
 
 namespace tachyon::tracker {
 
-// CameraTrack::apply_to is in camera_solver_keyframes.cpp
-// Math utilities are in camera_solver_math.cpp
+// CameraTrack::apply_to is defined in camera_solver_keyframes.cpp
+// Math utilities are defined in camera_solver_math.cpp
 
 } // namespace tachyon::tracker
