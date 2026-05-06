5230dd732513de5f9ff3b367a71b0a13e5a557c2
Your Name <you@example.com>
2026-05-06 21:44:51 +0200

    fix: drop orphan homography warp effect from build

--- cmake/tachyon/TachyonRenderer2DExtrasSources.cmake
+++ cmake/tachyon/TachyonRenderer2DExtrasSources.cmake
@@ -6,6 +6,5 @@ set(TachyonRenderer2DExtrasSources
     ${CMAKE_CURRENT_SOURCE_DIR}/renderer2d/effects/generators/particle_effects.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/renderer2d/effects/utility/number_counter_effect.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/renderer2d/evaluated_composition/rendering/primitives/media_card_mesh_builder.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/renderer2d/effects/distort/homography_warp_effect.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/renderer2d/spec/project_card.cpp
 )
